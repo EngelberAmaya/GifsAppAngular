a12db51dc51155ebb2b005a8e7efceb1678a914c
EngelberAmaya <engelberamaya@gmail.com>
2026-04-06 15:30:08 -0400

    Giphy API - GIF Service | GifService - Interfaces

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   companyName: 'Giphy',
   companyName2: 'App',
-  companySlogan: 'Maneja tus gifs'
+  companySlogan: 'Maneja tus gifs',
+  giphyApiKey: 'your_api_key_here',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
