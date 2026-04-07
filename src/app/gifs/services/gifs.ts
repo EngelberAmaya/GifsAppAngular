e40f3ebed019336eeb6ea03f96c586cb8a9d53d7
EngelberAmaya <engelberamaya@gmail.com>
2026-04-07 10:44:29 -0400

    HTTP GET request

--- src/app/gifs/services/gifs.ts
+++ src/app/gifs/services/gifs.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy';
+import { Gif } from '../interfaces/gif';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Gifs {
   private http = inject(HttpClient);
+  private trendingGifs = signal<Gif[]>([]);
+
+  constructor() {
+    this.loadTrendingGifs();
+  }
 
   loadTrendingGifs(){
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
@@ -15,6 +22,10 @@ export class Gifs {
         api_key: environment.giphyApiKey,
         limit: 20
       }
+    }).subscribe(resp => {
+      const gifs = GifMapper.mapGiphyItemsToGifsArray(resp.data);
+      this.trendingGifs.set(gifs);
+      console.log({gifs});
     })
   }
 
